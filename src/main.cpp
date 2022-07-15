7addcce3ce0b84da3a5a2a14d834f0608c44dc9e
Corentin Boireau <cboireau91@gmail.com>
2022-07-15 13:24:40 +0200

    Added progress indicator

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,19 @@ int main()
 {
     std::cout << "Ray Tracing in One Weekend !" << std::endl;
     
-    constexpr size_t imageWidth  = 256;
-    constexpr size_t imageHeight = 256;
+    constexpr size_t imageWidth  = 2560;
+    constexpr size_t imageHeight = 2560;
 
     std::vector<uint8_t> imageData;
     imageData.reserve(3 * imageWidth * imageHeight);
 
-    for (size_t i = 0; i < imageWidth; i++) 
+    for (size_t i = 0; i < imageHeight; i++)
     {
-        for (size_t j = 0; j < imageHeight; j++) 
+        std::cout << "\rRendering: " << i * 100 / imageHeight << "%" << std::flush;
+        for (size_t j = 0; j < imageWidth; j++) 
         {
-            double r = static_cast<double>(i) / (imageWidth - 1);
-            double g = static_cast<double>(j) / (imageHeight - 1);
+            double r = static_cast<double>(imageHeight - i) / (imageHeight - 1);
+            double g = static_cast<double>(j)               / (imageWidth - 1);
             double b = 0.25;
 
             uint8_t ir = static_cast<uint8_t>(255.999 * r);
@@ -31,6 +32,7 @@ int main()
             imageData.push_back(ib);
         }
     }
+    std::cout << "\33[2K\rRendering: Done." << std::endl;
     
     stbi_write_png("image.png", imageWidth, imageHeight, 3, imageData.data(), 0);
 }
